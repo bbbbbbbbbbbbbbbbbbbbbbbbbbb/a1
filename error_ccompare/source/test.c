155cf9f131b2d043973bb8298a085f220bff1bd4
wasdx1107 <wasdx1107@gmail.com>
2017-01-12 19:45:56 +0800

    6

---
+++ error_ccompare/source/test.c
@@ -0,0 +1,30 @@
+int compare(int your_answer[])
+{
+	int a_count = 0, b_count = 0, i, j;
+
+	for (i = 0; i < DIGITS - 1; i++)
+	{
+		if (your_answer[i] == question[i])
+		{
+			a_count++
+		}
+		else
+		{
+			for (j = 0; j < DIGITS; j++)
+			{
+				if (your_answer[i] == question[j] && j != i)
+				{
+					b_count++;
+				}
+			}
+		}
+	}
+
+	printf("%dA%dB\n", a_count, b_count);
+	if (a_count == DIGITS)
+		return 1;
+	else
+	{
+		printf("請再猜一次 ^_^\n");
+		return 0;
+	}
